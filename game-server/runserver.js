1b7679b7bbe3bbfb68a6769392909869048ca44b
赖信涛 <laixintao1995@163.com>
2015-10-26 14:30:18 +0800

    重构函数，处理第三个以上的人的情况。
    如果来第三个人则拒绝等待。将创建玩家的代码放入socket建立的时候

--- game-server/runserver.js
+++ game-server/runserver.js
@@ -2,48 +2,59 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 3333 });
 
-function Player(player,speedX,speedY,shareSpeedX,shareSpeedY,ball){
+var Player = function(player,speedX,speedY,shareSpeedX,shareSpeedY,ball){
     this.player = player;
     this.speedX = speedX;
     this.speedY = speedY;
     this.shareSpeedX = shareSpeedX;
     this.shareSpeedY = shareSpeedY;
     this.ball = ball;
-    this.toJSON = function(){
-        var str;
-        str = '{"player":"'+this.player+
-            '","speedX":"'+this.speedX+
-            '","speedY":"'+this.speedY+
-            '","shareSpeedX":"'+this.shareSpeedX+
-            '","shareSpeedY":"'+this.shareSpeedY+
-            '","ball":"'+this.ball+
-            '"}';
-        return str;
-    }
-}
+};
 
+Player.prototype.toJSON = function(){
+    var str;
+    str = '{"player":"'+this.player+
+        '","speedX":"'+this.speedX+
+        '","speedY":"'+this.speedY+
+        '","shareSpeedX":"'+this.shareSpeedX+
+        '","shareSpeedY":"'+this.shareSpeedY+
+        '","ball":"'+this.ball+
+        '"}';
+    return str;
+};
 
+Player.prototype.set_msg = function(player,speedX,speedY,shareSpeedX,shareSpeedY,ball){
+    this.player = player;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.shareSpeedX = shareSpeedX;
+    this.shareSpeedY = shareSpeedY;
+    this.ball = ball;
+};
 
 var num_players = 0;
 
-var playerA,playerB;
+var playerA;
+var playerB;
 
 wss.on('connection', function connection(ws){
 
-    //ws.on('open',function open(){
-    //    console.log("new player in..."+num_players);
-    //    if(num_players == 0) {
-    //        playerA = new Player();
-    //        num_players++;
-    //    }
-    //    else if(num_players == 1) {
-    //        playerB = new Player();
-    //        num_players++;
-    //    }
-    //    else
-    //        console.log('too many players...');
-    //    console.log(num_players);
-    //});
+    //使用w3c的onopen实现监听
+    //只有先用on再用onopen能正常工作
+    //》》》我也不知道为什么
+    ws.on('open',function(){
+        num_players ++;
+       console.log("open...");
+        if(num_players == 1){
+            playerA = new Player();
+        }else if(num_players == 2){
+            playerB = new Player();
+        }else{
+            console.log('reject new player...');
+            this.terminate();
+        }
+    });
+    ws.onopen(function(){});
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
@@ -51,78 +62,29 @@ wss.on('connection', function connection(ws){
         var tmp = new Array();
         tmp = JSON.parse(message);
         name = tmp['player'];
-        if(num_players == 0) {
-            playerA = new Player();
-            playerA.player = tmp['player'];
-            playerA.speedX = tmp['speedX'];
-            playerA.speedY = tmp['speedY'];
-            playerA.shareSpeedX = tmp['shareSpeedX'];
-            playerA.shareSpeedY = tmp['shareSpeedY'];
-            playerA.ball = tmp['ball'];
+        if(num_players == 1) {
             msg_send = "wait";
-            num_players ++;
+            playerA = new Player(tmp['player'],tmp['speedX'],tmp['speedY'],
+                tmp['shareSpeedX'],tmp['shareSpeedY'],tmp['ball']);
             console.log(num_players);
-        }else if(num_players == 1 && name != playerA.player) {
-            playerB = new Player();
-            playerB.player = tmp['player'];
-            playerB.speedX = tmp['speedX'];
-            playerB.speedY = tmp['speedY'];
-            playerB.shareSpeedX = tmp['shareSpeedX'];
-            playerB.shareSpeedY = tmp['shareSpeedY'];
-            playerB.ball = tmp['ball'];
-            //console.log("------------");
-            //console.log(playerA.toJSON());
-            num_players++;
+        }else if(num_players == 2 && name != playerA.player) {
+            playerB = new Player(tmp['player'],tmp['speedX'],tmp['speedY'],
+                tmp['shareSpeedX'],tmp['shareSpeedY'],tmp['ball']);
             console.log(num_players);
-            msg_send = '[{"player":"'+playerA.player+
-                '","speedX":"'+playerA.speedX+
-                '","speedY":"'+playerA.speedY+
-                '","shareSpeedX":"'+playerA.shareSpeedX+
-                '","shareSpeedY":"'+playerA.shareSpeedY+
-                '","ball":"'+playerA.ball+
-                '"},{"player":"'+playerB.player+
-                '","speedX":"'+playerB.speedX+
-                '","speedY":"'+playerB.speedY+
-                '","shareSpeedX":"'+playerB.shareSpeedX+
-                '","shareSpeedY":"'+playerB.shareSpeedY+
-                '","ball":"'+playerB.ball+
-                '"}]';
+            msg_send = '[' + playerA.toJSON() + ',' + playerB.toJSON() + ']';
         }else if(num_players == 2){
             if(name == playerA.player){
                 console.log("A move...");
-                playerA.player = tmp['player'];
-                playerA.speedX = tmp['speedX'];
-                playerA.speedY = tmp['speedY'];
-                playerA.shareSpeedX = tmp['shareSpeedX'];
-                playerA.shareSpeedY = tmp['shareSpeedY'];
-                playerA.ball = tmp['ball'];
+                playerA.set_msg(tmp['player'],tmp['speedX'],tmp['speedY'],
+                    tmp['shareSpeedX'],tmp['shareSpeedY'],tmp['ball']);
             }else{
                 console.log('B move...');
-                playerB.player = tmp['player'];
-                playerB.speedX = tmp['speedX'];
-                playerB.speedY = tmp['speedY'];
-                playerB.shareSpeedX = tmp['shareSpeedX'];
-                playerB.shareSpeedY = tmp['shareSpeedY'];
-                playerB.ball = tmp['ball'];
+                playerB.set_msg(tmp['player'],tmp['speedX'],tmp['speedY'],
+                    tmp['shareSpeedX'],tmp['shareSpeedY'],tmp['ball']);
             }
-            msg_send = '[{"player":"'+playerA.player+
-                '","speedX":"'+playerA.speedX+
-                '","speedY":"'+playerA.speedY+
-                '","shareSpeedX":"'+playerA.shareSpeedX+
-                '","shareSpeedY":"'+playerA.shareSpeedY+
-                '","ball":"'+playerA.ball+
-                '"},{"player":"'+playerB.player+
-                '","speedX":"'+playerB.speedX+
-                '","speedY":"'+playerB.speedY+
-                '","shareSpeedX":"'+playerB.shareSpeedX+
-                '","shareSpeedY":"'+playerB.shareSpeedY+
-                '","ball":"'+playerB.ball+
-                '"}]';
-
+            msg_send = '[' + playerA.toJSON() + ',' + playerB.toJSON() + ']';
         }
         console.log(msg_send);
-        //console.log(playerA.toJSON());
-        //console.log(playerB.toJSON());
         wss.clients.forEach(function each(client) {
             client.send(msg_send);
         });
@@ -130,14 +92,6 @@ wss.on('connection', function connection(ws){
 
     ws.on('close',function close(code,message){
         num_players--;
-       console.log(code + "player leave..." + num_players);
+       console.log('code: '+ code + "... player leave..." + num_players);
     });
 });
-
-//wss.on('error',function error(ws){
-//    console.log('error...');
-//});
-//
-//wss.on('header',function header(ws){
-//    console.log(ws);
-//});
